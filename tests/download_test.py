1408bc4deeb4e51023f4d87fa2a02d029cdb1d9e
Aerex <nanaus13@yahoo.com>
2017-12-03 14:42:25 -0600

    feat: added redis and celery

--- tests/download_test.py
+++ tests/download_test.py
@@ -7,14 +7,18 @@ from pytube import YouTube
 
 def mockCallback(path):
     pass
-class mockVideo():
+class mockVideo(object):
+    def __init__(self, filename, resolution):
+        self.filename = filename
+        self.resolution = resolution
     def download(self, dir, on_finish=None):
         if on_finish:
             on_finish(dir)
 
 def test_download_highest_quality(mocker):
     highest = '1080p'
-    videos = [
+    videos = []
+    videosConfig = [
         {
             'resolution': '1080p',
             'filename': 'sample.mp4'
@@ -28,7 +32,11 @@ def test_download_highest_quality(mocker):
             'filename': 'sample.mp4'
         }
     ]
+    for x in range(3):
+        videos.append(mockVideo(videosConfig[x]['filename'], videosConfig[x]['resolution']))
 
+    print 'hello\n'
+    print videosConfig[0]['filename']
     url = 'https://google.com'
     ext = 'mp4'
     quality = 'highest'
@@ -47,7 +55,49 @@ def test_download_highest_quality(mocker):
     yt = YoutubeDownloader(url)
     yt.download(ext, quality, dir, callback=mockCallback)
     YouTube.get.assert_called_with(ext, highest)
-    os.path.exists.assert_called_with('{0}/{1}.{2}'.format(dir, videos[0]['filename'], ext))
+    os.path.exists.assert_called_with('{0}/{1}.{2}'.format(dir, videos[0].filename, ext))
+
+def test_download_lowest_quality(mocker):
+    lowest = '480p'
+    videos = []
+    videosConfig = [
+        {
+            'resolution': '1080p',
+            'filename': 'sample.mp4'
+        },
+        {
+            'resolution': '720p',
+            'filename': 'sample.mp4'
+        },
+        {
+            'resolution': '480p',
+            'filename': 'sample.mp4'
+        }
+    ]
+
+
+    for x in range(3):
+        videos.append(mockVideo(videosConfig[x]['filename'], videosConfig[x]['resolution']))
+
+    url = 'https://google.com'
+    ext = 'mp4'
+    quality = 'lowest'
+    dir = '/tmp'
+    mocker.patch.object(YouTube, '__init__')
+    mocker.patch('os.path.exists')
+    mocker.patch.object(YouTube, 'get')
+    mocker.patch.object(YouTube, 'filter')
+    mocker.patch.object(mockVideo, 'download')
+
+    YouTube.get.return_value = videos[2]
+    YouTube.__init__.return_value = None
+    YouTube.filter.return_value = videos
+    os.path.exists.return_value = True
+
+    yt = YoutubeDownloader(url)
+    yt.download(ext, quality, dir, callback=mockCallback)
+    YouTube.get.assert_called_with(ext, lowest)
+    os.path.exists.assert_called_with('{0}/{1}.{2}'.format(dir, videosConfig[2]['filename'], ext))
 
 
 
